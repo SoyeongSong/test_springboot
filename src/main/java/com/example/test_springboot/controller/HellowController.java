7e779e425a2e871c856ddcde2b4b2d99ca05dd46
Your Name <ssy0316@gmail.com>
2019-08-14 16:28:17 +0900

    fix message

--- src/main/java/com/example/test_springboot/controller/HellowController.java
+++ src/main/java/com/example/test_springboot/controller/HellowController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HellowController {
  @RequestMapping(value = "/resthellow")
  public String hellow() {
- return "welcome Hellow World !! ";
+ return "Welcome Hello My Funny World!! ";
  }
 }
